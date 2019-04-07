7ea43b277cf41ed66b532f7be70b3b4ce9b61cfb
MrWolverineDude <NicolasWilliams2016@gmail.com>
2019-04-07 15:59:29 -0400

    Now moving to mac

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 var myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello, World!';
 
+// An event
 document.querySelector('html').onclick = function () {
     alert('Ouch! Stop poking me!');
 }
