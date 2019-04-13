ca626021c1aba92a39311d98cc6ffd65aa2665e3
MrWolverineDude <NicolasWilliams2016@gmail.com>
2019-04-12 20:26:51 -0400

    It works!

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,5 @@
-var myImage = document.querySelector('img');
+var text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
-myImage.onclick = function() {
-  mySrc = myImage.getAttribute('src');
-  if (mySrc === 'images/firefox_logo.png') {
-    myImage.setAttribute('src', 'images/firefox2.png');
-  } else {
-    myImage.setAttribute('src', 'images/firefox_logo.png');
-  }
-}
+var targets = document.getElementById("projectInfo");
+
+targets.textContent = text;
